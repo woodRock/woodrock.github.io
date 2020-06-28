ca4991bed1a46c0d403ecd73bfcba0663473028d
Jesse Wood <j.r.h.wood98@gmail.com>
2020-06-29 00:03:57 +1200

    Add props.firebase to the dependency arrays for the hooks

--- src/app/components/Blog/index.js
+++ src/app/components/Blog/index.js
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from 'react';
+import React, {useState, useEffect} from 'react';
 import ReactMarkdown from 'react-markdown';
-import { withFirebase } from '../../util/Firebase';
+import {withFirebase} from '../../util/Firebase';
 import TimeAgo from '../../util/TimeAgo';
 import logo from '../../../assets/logo.png';
 import Loading from '../Loading';
@@ -10,66 +10,52 @@ const BlogPage = (props) => {
   const [blog, setBlog] = useState([]);
 
   useEffect(() => {
-    const blogs = () => {
-      props.firebase.blog().orderBy('title').get().then(
-        querySnapshot => {
-            querySnapshot.forEach(doc => {
-              const data = {
-                'id': doc.id,
-                'title': doc.data().title,
-                'time': doc.data().time,
-                'markdown': doc.data().markdown
-              };
-              setBlog(prevBlog => [...prevBlog, data]);
-            })
-          }
-        )
-      };
-      blogs();
-    }
-  );
+    props.firebase.blog().orderBy('title').get().then(querySnapshot => {
+      querySnapshot.forEach(doc => {
+        const data = {
+          'id': doc.id,
+          'title': doc.data().title,
+          'time': doc.data().time,
+          'markdown': doc.data().markdown
+        };
+        setBlog(prevBlog => [
+          ...prevBlog,
+          data
+        ]);
+      })
+    })
+  }, [props.firebase]);
 
-  return (
-    <div className="blog">
-      <h1>Blog</h1>
-      {!blog.length && <Loading/>}
-      {blog ? (
-        <BlogList blog={blog.sort((a,b) => new Date(b.time) - new Date(a.time))} />
-      ) : (
-        <div>There are no blog posts ...</div>
-      )}
-    </div>
-  );
+  return (<div className="blog">
+    <h1>Blog</h1>
+    {!blog.length && <Loading/>}
+    {
+      blog
+        ? (<BlogList blog={blog.sort((a, b) => new Date(b.time) - new Date(a.time))}/>)
+        : (<div>There are no blog posts ...</div>)
+    }
+  </div>);
 }
 
-const BlogList = ({ blog }) => (
-  <div>
-    {blog.map(s => (
-      <BlogItem key={s.id} blog={s} />
-    ))}
-  </div>
-);
+const BlogList = ({blog}) => (<div>
+  {blog.map(s => (<BlogItem key={s.id} blog={s}/>))}
+</div>);
 
-const BlogItem = ({ blog }) => (
-  <div className="blog-post twitter-style-border">
-    <div className="title">
-      <img class="logo" src={logo} alt="woodRock github logo"/>
-      <span class="blog-title-text">
-        woodRock •
-          <i>
-            <span className="secondary"> {TimeAgo({date: blog.time})}</span>
-          </i>
-      </span>
-    </div>
-    <div className="blog-content">
-      <h2>{blog.title}</h2>
-      <ReactMarkdown
-        source={
-          (blog.markdown + "").replace(/\\n/g,"\n\n")
-        }
-      />
-    </div>
+const BlogItem = ({blog}) => (<div className="blog-post twitter-style-border">
+  <div className="title">
+    <img class="logo" src={logo} alt="woodRock github logo"/>
+    <span class="blog-title-text">
+      woodRock •
+      <i>
+        <span className="secondary">
+          {TimeAgo({date: blog.time})}</span>
+      </i>
+    </span>
   </div>
-);
+  <div className="blog-content">
+    <h2>{blog.title}</h2>
+    <ReactMarkdown source={(blog.markdown + "").replace(/\\n/g, "\n\n")}/>
+  </div>
+</div>);
 
 export default withFirebase(BlogPage);
--- src/app/components/Projects/index.js
+++ src/app/components/Projects/index.js
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from 'react';
+import React, {useState, useEffect} from 'react';
 import ReactMarkdown from 'react-markdown';
-import { withFirebase } from '../../util/Firebase';
+import {withFirebase} from '../../util/Firebase';
 import TimeAgo from '../../util/TimeAgo';
 import logo from '../../../assets/logo.png';
 import Loading from '../Loading';
@@ -9,73 +9,65 @@ import './index.css';
 const ProjectsPage = (props) => {
   const [projects, setProjects] = useState([]);
 
-  useEffect( () => {
-    const projects = () => {
-      props.firebase.projects().orderBy('time').get().then(
-        querySnapshot => {
-          querySnapshot.forEach(doc => {
-            const data = {
-              'id': doc.id,
-              'title': doc.data().title,
-              'link': doc.data().link,
-              'image': doc.data().image,
-              'description': doc.data().description,
-              'time': doc.data().time
-            };
-            setProjects(prevProjects => [...prevProjects, data]);
-          })
-        }
-      )
-    };
-    projects();
-  }
-  );
+  useEffect(() => {
+    props.firebase.projects().orderBy('time').get().then(querySnapshot => {
+      querySnapshot.forEach(doc => {
+        const data = {
+          'id': doc.id,
+          'title': doc.data().title,
+          'link': doc.data().link,
+          'image': doc.data().image,
+          'description': doc.data().description,
+          'time': doc.data().time
+        };
+        setProjects(prevProjects => [
+          ...prevProjects,
+          data
+        ]);
+      })
+    })
+  }, [props.firebase]);
 
-  return (
-    <div>
-      <h1>Projects</h1>
-      {!projects.length && <Loading/>}
-      {projects ? (
-        <ProjectList projects={projects.sort((a,b) => new Date(b.time) - new Date(a.time))} />
-      ) : (
-        <div>There are no projects ...</div>
-      )}
-    </div>
-  );
+  return (<div>
+    <h1>Projects</h1>
+    {!projects.length && <Loading/>}
+    {
+      projects
+        ? (<ProjectList projects={projects.sort((a, b) => new Date(b.time) - new Date(a.time))}/>)
+        : (<div>There are no projects ...</div>)
+    }
+  </div>);
 }
 
-const ProjectList = ({ projects }) => (
-  <div>
-    {projects.map(project => (
-      <ProjectItem key={project.id} project={project} />
-    ))}
-  </div>
-);
+const ProjectList = ({projects}) => (<div>
+  {projects.map(project => (<ProjectItem key={project.id} project={project}/>))}
+</div>);
 
-const ProjectItem = ({ project }) => (
-  <div className="project-container">
-    <div className="title">
-      <img class="logo" src={logo} alt="woodRock github logo"/>
-      <span className="project-title-text">
-        <span className="header"> @woodRock</span> •
-        <i className="secondary"> {TimeAgo( { date: project.time} )}</i>
-      </span>
-      <div className="description text">
-        <h2>{project.title}</h2>
-        <ReactMarkdown source={project.description}></ReactMarkdown>
-      </div>
-    </div>
-    <div className="project">
-      <a href={project.link}>
-        <img width="100%" height="width" src={project.image} alt={project.title}/>
-      </a>
-      <div>
-        <a href={project.link} className="github-link">
-          <i className="fa fa-github"></i>
-        </a>
-      </div>
+const ProjectItem = ({project}) => (<div className="project-container">
+  <div className="title">
+    <img class="logo" src={logo} alt="woodRock github logo"/>
+    <span className="project-title-text">
+      <span className="header">
+        @woodRock</span>
+      •
+      <i className="secondary">
+        {TimeAgo({date: project.time})}</i>
+    </span>
+    <div className="description text">
+      <h2>{project.title}</h2>
+      <ReactMarkdown source={project.description}></ReactMarkdown>
     </div>
   </div>
-);
+  <div className="project">
+    <a href={project.link}>
+      <img width="100%" height="width" src={project.image} alt={project.title}/>
+    </a>
+    <div>
+      <a href={project.link} className="github-link">
+        <i className="fa fa-github"></i>
+      </a>
+    </div>
+  </div>
+</div>);
 
 export default withFirebase(ProjectsPage);
