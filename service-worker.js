037c301c687e8a783137622473cdf188179e4558
woodRock <woodRock@users.noreply.github.com>
2020-08-23 05:48:56 +0000

    Deploying to master from  @ 389c3c05988fae7a5aabea77d07f728b15d79be8 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1b77c043830fa448be27d98cb3b315ac.js"
+  "/precache-manifest.71651f3052ddb9abd4feda62c2d34c84.js"
 );
 
 self.addEventListener('message', (event) => {
