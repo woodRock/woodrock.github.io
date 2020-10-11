2d9830821387c899aed206b3367322cceb56b88c
Jesse Wood <j.r.h.wood98@gmail.com>
2020-10-12 03:25:45 +1300

    Add documentation to theme context.

--- src/api/Theme.js
+++ src/api/Theme.js
@@ -1,13 +1,50 @@
 import React, { createContext, useContext, useReducer } from "react";
 
+/**
+ * This stores the state for the theme context.
+ * That is a string value, which is `light` or `dark`.
+ */
 const ThemeContextState = createContext([]);
 
+/**
+ * This stores the dispatch for the theme context.
+ * It handles the actions we can perform on the state.
+ * We can `toggle` the state, from `light` to `dark`, or vice versa.
+ */
 const ThemeContextDispatch = createContext([]);
 
-const useThemeState = () => useContext(ThemeContextState);
+/**
+ * Syntax sugar for using the context theme's state
+ * Additional error handling to provide context sensitive exceptions.
+ */
+const useThemeState = () => {
+  const context = useContext(ThemeContextState);
+  if (context === undefined) {
+    throw new Error(
+      "useThemeState must be used within a ThemeContextState context."
+    );
+  }
+  return context;
+};
 
-const useThemeDispatcher = () => useContext(ThemeContextDispatch);
+/**
+ * Syntax sugar for using the context theme's dispatch.
+ * Additional error handling to provide context sensitive exceptions.
+ */
+const useThemeDispatcher = () => {
+  const context = useContext(ThemeContextDispatch);
+  if (context === undefined) {
+    throw new Error(
+      "useThemeDispatcher must be used within a ThemeContextDispatch context."
+    );
+  }
+  return context;
+};
 
+/**
+ * Performs actions of the state of the theme context.
+ * Here we can toggle the existing state of the theme.
+ */
 const reducer = (state, action) => {
   switch (action.type) {
     case "toggle":
@@ -18,6 +55,11 @@ const reducer = (state, action) => {
   }
 };
 
+/**
+ * Provider for the theme context.
+ * This must be supplied at a parent or node that wishes to use it.
+ * We nest the dispatch inside the state, order is important here.
+ */
 const ThemeProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, { theme: "dark" });
   return (
