15650062f5d4946ec4922ff73188c4fbd10cea26
woodrock <j.r.h.wood98@gmail.com>
2021-02-13 15:32:26 +1300

    Document firebase config with a comment.

--- src/api/config.js
+++ src/api/config.js
@@ -1,3 +1,6 @@
+/**
+ * Firebase configuration details. Should be a secret in the future.
+ */
 export default {
   apiKey: "AIzaSyA6w14wLmGD4M3zYhUsWc_IZMsAO6Cs7Ng",
   authDomain: "portfolio-7b51b.firebaseapp.com",
@@ -6,5 +9,5 @@ export default {
   storageBucket: "portfolio-7b51b.appspot.com",
   messagingSenderId: "888392851400",
   appId: "1:888392851400:web:c91412c8602e7f2e6f2afd",
-  measurementId: "G-N4G5H32S57"
+  measurementId: "G-N4G5H32S57",
 };
