e31333ecd9ac44104a9940829e11ce0709b2de01
woodRock <j.r.h.wood98@gmail.com>
2023-11-28 16:03:32 +1300

    Move dice

--- src/components/Dice.js
+++ src/components/Dice.js
@@ -9,21 +9,37 @@
  * By default this die, links to the home page when clicked.
  */
 
-import React from "react";
+import React, {useState} from "react";
 import { Link } from "react-router-dom";
 import { Canvas } from "@react-three/fiber";
 import Box from "./Box";
 
-const Dice = () => (
+const Dice = () => {
+
+  dice_center, set_dice_center = useState([0, 0, 0]);
+
+  // Move the dice randomly around the screen.
+  const move_dice = () => {
+    const x = Math.random() * 10 - 5;
+    const y = Math.random() * 10 - 5;
+    const z = Math.random() * 10 - 5;
+    set_dice_center([x, y, z]);
+  };
+
+  // Move the dice every 0.1 seconds.
+  setInterval(move_dice, `100`);
+
+  return (
   <React.Fragment>
     <Link to="/">Home</Link>
     <Canvas>
       <ambientLight intensity={0.5} />
       <spotLight angle={0.15} penumbra={1} position={[10, 10, 10]} />
       <pointLight position={[-10, -10, -10]} />
-      <Box position={[0, 0, 0]} />
+      <Box position={dice_center} />
     </Canvas>
   </React.Fragment>
-);
+  );
+}
 
 export default Dice;
